d0d65c678b8221f26670a1ff3e507305a4f9bdfd
sohyeonyun <57862001+sohyeonyun@users.noreply.github.com>
2021-01-05 03:46:28 +0900

    ex) exception, finally

--- chapter10.py
+++ chapter10.py
@@ -1,57 +1,57 @@
 # 예외처리, 에러 발생시키기, 사용자 정의 예외처리, finally
 
 
-# ### 예외처리
-# try:
-#     print("나누기 전용 계산기입니다.")
-#     nums = []
-#     nums.append(int(input("첫 번째 숫자를 입력하세요 : ")))
-#     nums.append(int(input("첫 번째 숫자를 입력하세요 : "))) # 숫자 아닐 시 ValueError
-#     #nums.append(int(nums[0] / nums[1]))
-#     print("{0} / {1} = {2}".format(nums[0], nums[1], nums[2]))
-# except ValueError:
-#     print("에러! 잘못된 값을 입력하였습니다.")
-# except ZeroDivisionError as err:
-#     print(err) # 발생하는 에러 그대로 출력
-# except Exception as err: # 위 에러들을 제외한 에러 처리
-#     print("알 수 없는 에러가 발생하였습니다.")
-#     print(err)
+### 예외처리
+try:
+    print("나누기 전용 계산기입니다.")
+    nums = []
+    nums.append(int(input("첫 번째 숫자를 입력하세요 : ")))
+    nums.append(int(input("첫 번째 숫자를 입력하세요 : "))) # 숫자 아닐 시 ValueError
+    #nums.append(int(nums[0] / nums[1]))
+    print("{0} / {1} = {2}".format(nums[0], nums[1], nums[2]))
+except ValueError:
+    print("에러! 잘못된 값을 입력하였습니다.")
+except ZeroDivisionError as err:
+    print(err) # 발생하는 에러 그대로 출력
+except Exception as err: # 위 에러들을 제외한 에러 처리
+    print("알 수 없는 에러가 발생하였습니다.")
+    print(err)
 
 
-# ### 에러 발생시키기
-# try:
-#     print("한 자리 숫자 나누기 전용 계산기입니다.")
-#     num1 = int(input("첫 번째 숫자를 입력하세요 : "))
-#     num2 = int(input("첫 번째 숫자를 입력하세요 : "))
-#     if num1 >= 10 or num2 >= 10: # 한 자리 아니면 에러 발생시킴
-#         raise ValueError
-#     print("{0} / {1} = {2}".format(num1, num2, int(num1 / num2)))
-# except ValueError:
-#     print("잘못된 값을 입력하였습니다. 한 자리 숫자만 입력하세요.")
+### 에러 발생시키기
+try:
+    print("한 자리 숫자 나누기 전용 계산기입니다.")
+    num1 = int(input("첫 번째 숫자를 입력하세요 : "))
+    num2 = int(input("첫 번째 숫자를 입력하세요 : "))
+    if num1 >= 10 or num2 >= 10: # 한 자리 아니면 에러 발생시킴
+        raise ValueError
+    print("{0} / {1} = {2}".format(num1, num2, int(num1 / num2)))
+except ValueError:
+    print("잘못된 값을 입력하였습니다. 한 자리 숫자만 입력하세요.")
 
 
-# ### 사용자 정의 예외처리 - 에러를 직접 정의
-# class BigNumberError(Exception):
-#     def __init__(self, msg):
-#         self.msg = msg
+### 사용자 정의 예외처리 - 에러를 직접 정의
+class BigNumberError(Exception):
+    def __init__(self, msg):
+        self.msg = msg
 
-#     def __str__(self):
-#         return self.msg
+    def __str__(self):
+        return self.msg
 
-# try:
-#     print("한 자리 숫자 나누기 전용 계산기입니다.")
-#     num1 = int(input("첫 번째 숫자를 입력하세요 : "))
-#     num2 = int(input("첫 번째 숫자를 입력하세요 : "))
-#     if num1 >= 10 or num2 >= 10: # 한 자리 아니면 에러 발생시킴
-#         raise BigNumberError("입력값 : {0}, {1}".format(num1, num2))
-#     print("{0} / {1} = {2}".format(num1, num2, int(num1 / num2)))
-# except ValueError:
-#     print("잘못된 값을 입력하였습니다. 한 자리 숫자만 입력하세요.")
-# except BigNumberError as err:
-#     print("에러가 발생하였습니다. 한 자리 숫자만 입력하세요.")
-#     print(err)
-# finally: # try든 except든 끝나고 무조건 실행
-#     print("계산기를 이용해 주셔서 감사합니다.")
+try:
+    print("한 자리 숫자 나누기 전용 계산기입니다.")
+    num1 = int(input("첫 번째 숫자를 입력하세요 : "))
+    num2 = int(input("첫 번째 숫자를 입력하세요 : "))
+    if num1 >= 10 or num2 >= 10: # 한 자리 아니면 에러 발생시킴
+        raise BigNumberError("입력값 : {0}, {1}".format(num1, num2))
+    print("{0} / {1} = {2}".format(num1, num2, int(num1 / num2)))
+except ValueError:
+    print("잘못된 값을 입력하였습니다. 한 자리 숫자만 입력하세요.")
+except BigNumberError as err:
+    print("에러가 발생하였습니다. 한 자리 숫자만 입력하세요.")
+    print(err)
+finally: # try든 except든 끝나고 무조건 실행
+    print("계산기를 이용해 주셔서 감사합니다.")
 
 
 ### 퀴즈
